338f68d0fdade14902ddfadbb0547fd51343d6ae
Timothy Choe <timothychoe@MacBook-Pro.local>
2020-08-13 16:28:40 -0400

    Updates

---
+++ precache-manifest.460d1777b02bb93b1e9e24800f418639.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6fcbca5c7ddbb8535e32199e82f520",
+    "url": "/Untitled-/index.html"
+  },
+  {
+    "revision": "c2845b021df91ba92a4d",
+    "url": "/Untitled-/static/css/main.6b9b8850.chunk.css"
+  },
+  {
+    "revision": "5e0475a7e50cc2dfe85a",
+    "url": "/Untitled-/static/js/2.79bde8d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Untitled-/static/js/2.79bde8d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2845b021df91ba92a4d",
+    "url": "/Untitled-/static/js/main.f1eea5c8.chunk.js"
+  },
+  {
+    "revision": "8b9f1e662a8db5e3d8e2",
+    "url": "/Untitled-/static/js/runtime-main.4afeba30.js"
+  },
+  {
+    "revision": "faed674173676078ad6d8fd2cf080fff",
+    "url": "/Untitled-/static/media/arrows.faed6741.png"
+  },
+  {
+    "revision": "e9814d34dbf22cf145dfb53bbdc139cf",
+    "url": "/Untitled-/static/media/search.e9814d34.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5bfb758f1af615d111865e341d97d58.js"
+  "/Untitled-/precache-manifest.460d1777b02bb93b1e9e24800f418639.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Untitled-/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
