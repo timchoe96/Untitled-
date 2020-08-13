2687dcbe7002ea364df73f26cbe565038107a842
Timothy Choe <timothychoe@MacBook-Pro.local>
2020-08-13 02:59:45 -0400

    Updates

---
+++ precache-manifest.a4e206e9ff3a3d9b4d2dacc8a236b0a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b32739d4ee8e28e4ef7c7d4a7071746",
+    "url": "/Untitled-/index.html"
+  },
+  {
+    "revision": "7ad5c31e358c4e1e6249",
+    "url": "/Untitled-/static/css/main.abca8b41.chunk.css"
+  },
+  {
+    "revision": "2de71ec5008035d0d8b1",
+    "url": "/Untitled-/static/js/2.1ed7586b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Untitled-/static/js/2.1ed7586b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ad5c31e358c4e1e6249",
+    "url": "/Untitled-/static/js/main.39c8fb9a.chunk.js"
+  },
+  {
+    "revision": "8b9f1e662a8db5e3d8e2",
+    "url": "/Untitled-/static/js/runtime-main.4afeba30.js"
+  },
+  {
+    "revision": "faed674173676078ad6d8fd2cf080fff",
+    "url": "/Untitled-/static/media/arrows.faed6741.png"
+  },
+  {
+    "revision": "e9814d34dbf22cf145dfb53bbdc139cf",
+    "url": "/Untitled-/static/media/search.e9814d34.svg"
+  }
+]);
