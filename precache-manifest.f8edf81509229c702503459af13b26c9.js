f72bf00ae0db5638b89845c3a6e5767249ce200d
Timothy Choe <timothychoe@MacBook-Pro.local>
2020-08-15 21:28:35 -0400

    Updates

---
+++ precache-manifest.f8edf81509229c702503459af13b26c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0018e35faadcde9138e9b8284c7949",
+    "url": "/index.html"
+  },
+  {
+    "revision": "299fa5c19134794130f8",
+    "url": "/static/css/main.5b06c36a.chunk.css"
+  },
+  {
+    "revision": "4f5686c9a28d11a1fd06",
+    "url": "/static/js/2.fe419512.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.fe419512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299fa5c19134794130f8",
+    "url": "/static/js/main.97b06dc9.chunk.js"
+  },
+  {
+    "revision": "9f10ad7796608ffb84b7",
+    "url": "/static/js/runtime-main.1b50fb31.js"
+  },
+  {
+    "revision": "faed674173676078ad6d8fd2cf080fff",
+    "url": "/static/media/arrows.faed6741.png"
+  },
+  {
+    "revision": "e9814d34dbf22cf145dfb53bbdc139cf",
+    "url": "/static/media/search.e9814d34.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Untitled-/precache-manifest.4660a62f26e55bcada695c7c69d45928.js"
+  "/precache-manifest.f8edf81509229c702503459af13b26c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Untitled-/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
